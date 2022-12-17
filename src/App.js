f337d301a300b85f905ebb27045e63eaa78180a8
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-12-17 11:24:26 +0530

    add lifecycle method ft. API fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Component } from "react";
-
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -8,30 +6,35 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstname: "Prathamesh", lastname: "Belurkar" },
-      company: "ZTM",
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            👋 Hi {this.state.name.firstname} {this.state.name.lastname}, I work
-            at {this.state.company}.
-          </p>
-          <button
-            onClick={() => {
-              this.setState({
-                name: { firstname: "Pranav", lastname: "Belurkar" },
-              });
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
